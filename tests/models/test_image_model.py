a7852b6526d6addea750b801f96c726dc0cc62d6
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2024-11-26 11:36:25 +0000

    tests(image): update model tests

--- tests/models/test_image_model.py
+++ tests/models/test_image_model.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import pytest
 from neomodel import CardinalityViolation, RelationshipManager, RequiredProperty
 from pytest_cases import parametrize_with_cases
@@ -5,7 +7,7 @@ from pytest_cases import parametrize_with_cases
 from fed_reg.image.models import Image, PrivateImage, SharedImage
 from fed_reg.project.models import Project
 from fed_reg.service.models import ComputeService
-from tests.models.utils import image_model_dict, project_model_dict, service_model_dict
+from tests.models.utils import project_model_dict, service_model_dict
 
 
 @parametrize_with_cases("image_cls", has_tag=("class", "derived"))
@@ -17,28 +19,33 @@ def test_image_inheritance(
 
 
 @parametrize_with_cases("image_cls", has_tag="class")
-@parametrize_with_cases("attr", has_tag="attr")
-def test_image_attr(
-    image_cls: type[Image] | type[PrivateImage] | type[SharedImage], attr: str
+@parametrize_with_cases("data", has_tag=("dict", "valid"))
+def test_image_valid_attr(
+    image_cls: type[Image] | type[PrivateImage] | type[SharedImage],
+    data: dict[str, Any],
 ) -> None:
-    """Test attribute values (default and set).
+    """Test Image mandatory and optional attributes.
 
     Execute this test on Image, PrivateImage and SharedImage.
     """
-    d = image_model_dict(attr)
-    item = image_cls(**d)
+    item = image_cls(**data)
     assert item.uid is not None
-    assert item.description == d.get("description", "")
-    assert item.name == d.get("name")
-    assert item.uuid == d.get("uuid")
-    assert item.os_type is d.get("os_type", None)
-    assert item.os_distro is d.get("os_distro", None)
-    assert item.os_version is d.get("os_version", None)
-    assert item.architecture is d.get("architecture", None)
-    assert item.kernel_id is d.get("kernel_id", None)
-    assert item.cuda_support is d.get("cuda_support", False)
-    assert item.gpu_driver is d.get("gpu_driver", False)
-    assert item.tags == d.get("tags", [])
+    assert item.description == data.get("description", "")
+    assert item.name == data.get("name")
+    assert item.uuid == data.get("uuid")
+    assert item.os_type is data.get("os_type", None)
+    assert item.os_distro is data.get("os_distro", None)
+    assert item.os_version is data.get("os_version", None)
+    assert item.architecture is data.get("architecture", None)
+    assert item.kernel_id is data.get("kernel_id", None)
+    assert item.cuda_support is data.get("cuda_support", False)
+    assert item.gpu_driver is data.get("gpu_driver", False)
+    assert item.tags == data.get("tags", [])
+
+    if image_cls == SharedImage:
+        assert item.is_shared
+    if image_cls == PrivateImage:
+        assert not item.is_shared
 
     saved = item.save()
     assert saved.element_id_property
@@ -46,20 +53,18 @@ def test_image_attr(
 
 
 @parametrize_with_cases("image_cls", has_tag="class")
-@parametrize_with_cases("attr", has_tag=("attr", "mandatory"))
+@parametrize_with_cases("data", has_tag=("dict", "invalid"))
 def test_image_missing_mandatory_attr(
-    image_cls: type[Image] | type[PrivateImage] | type[SharedImage], attr: str
+    image_cls: type[Image] | type[PrivateImage] | type[SharedImage],
+    data: dict[str, Any],
 ) -> None:
     """Test Image required attributes.
 
     Creating a model without required values raises a RequiredProperty error.
     Execute this test on Image, PrivateImage and SharedImage.
     """
-    err_msg = f"property '{attr}' on objects of class {image_cls.__name__}"
-    d = image_model_dict()
-    d.pop(attr)
-    with pytest.raises(RequiredProperty, match=err_msg):
-        image_cls(**d).save()
+    with pytest.raises(RequiredProperty):
+        image_cls(**data).save()
 
 
 @parametrize_with_cases("image_model", has_tag="model")
@@ -76,6 +81,15 @@ def test_rel_def(image_model: Image | PrivateImage | SharedImage) -> None:
     assert image_model.services.definition
     assert image_model.services.definition["node_class"] == ComputeService
 
+    if isinstance(image_model, PrivateImage):
+        assert isinstance(image_model.projects, RelationshipManager)
+        assert image_model.projects.name
+        assert image_model.projects.source
+        assert isinstance(image_model.projects.source, PrivateImage)
+        assert image_model.projects.source.uid == image_model.uid
+        assert image_model.projects.definition
+        assert image_model.projects.definition["node_class"] == Project
+
 
 @parametrize_with_cases("image_model", has_tag="model")
 def test_required_rel(image_model: Image | PrivateImage | SharedImage) -> None:
@@ -90,6 +104,12 @@ def test_required_rel(image_model: Image | PrivateImage | SharedImage) -> None:
     with pytest.raises(CardinalityViolation):
         image_model.services.single()
 
+    if isinstance(image_model, PrivateImage):
+        with pytest.raises(CardinalityViolation):
+            image_model.projects.all()
+        with pytest.raises(CardinalityViolation):
+            image_model.projects.single()
+
 
 @parametrize_with_cases("image_model", has_tag="model")
 def test_single_linked_service(
@@ -98,7 +118,7 @@ def test_single_linked_service(
 ) -> None:
     """Verify `services` relationship works correctly.
 
-    Connect a single ComputeService to a Image.
+    Connect a single ComputeService to an Image.
     Execute this test on Image, PrivateImage and SharedImage.
     """
     r = image_model.services.connect(compute_service_model)
@@ -110,13 +130,30 @@ def test_single_linked_service(
     assert service.uid == compute_service_model.uid
 
 
+@parametrize_with_cases("image_model", has_tag=("model", "private"))
+def test_single_linked_project(
+    image_model: PrivateImage, project_model: Project
+) -> None:
+    """Verify `projects` relationship works correctly.
+
+    Connect a single Project to a PrivateImage.
+    """
+    r = image_model.projects.connect(project_model)
+    assert r is True
+
+    assert len(image_model.projects.all()) == 1
+    project = image_model.projects.single()
+    assert isinstance(project, Project)
+    assert project.uid == project_model.uid
+
+
 @parametrize_with_cases("image_model", has_tag="model")
 def test_multiple_linked_services(
     image_model: Image | PrivateImage | SharedImage,
 ) -> None:
     """Verify `services` relationship works correctly.
 
-    Connect a multiple ComputeService to a Image.
+    Connect a multiple ComputeService to an Image.
     Execute this test on Image, PrivateImage and SharedImage.
     """
     item = ComputeService(**service_model_dict()).save()
@@ -126,66 +163,14 @@ def test_multiple_linked_services(
     assert len(image_model.services.all()) == 2
 
 
-def test_shared_image_default_attr() -> None:
-    """Test SharedImage specific attribute values."""
-    d = image_model_dict()
-    item = SharedImage(**d)
-    assert item.is_shared is True
-
-
-def test_private_image_default_attr() -> None:
-    """Test SharedImage specific attribute values and relationships definition."""
-    d = image_model_dict()
-    item = PrivateImage(**d)
-    assert item.is_shared is False
-
-
-def test_private_image_rel_def(private_image_model: PrivateImage) -> None:
-    """Test relationships definition."""
-    assert isinstance(private_image_model.projects, RelationshipManager)
-    assert private_image_model.projects.name
-    assert private_image_model.projects.source
-    assert isinstance(private_image_model.projects.source, PrivateImage)
-    assert private_image_model.projects.source.uid == private_image_model.uid
-    assert private_image_model.projects.definition
-    assert private_image_model.projects.definition["node_class"] == Project
-
-
-def test_private_image_required_rel(private_image_model: PrivateImage) -> None:
-    """Test Model required relationships.
-
-    A model without required relationships can exist but when querying those values, it
-    raises a CardinalityViolation error.
-    """
-    with pytest.raises(CardinalityViolation):
-        private_image_model.projects.all()
-    with pytest.raises(CardinalityViolation):
-        private_image_model.projects.single()
-
-
-def test_single_linked_project(
-    private_image_model: PrivateImage, project_model: Project
-) -> None:
-    """Verify `projects` relationship works correctly.
-
-    Connect a single Project to a PrivateImage.
-    """
-    r = private_image_model.projects.connect(project_model)
-    assert r is True
-
-    assert len(private_image_model.projects.all()) == 1
-    project = private_image_model.projects.single()
-    assert isinstance(project, Project)
-    assert project.uid == project_model.uid
-
-
-def test_multiple_linked_projects(private_image_model: PrivateImage) -> None:
+@parametrize_with_cases("image_model", has_tag=("model", "private"))
+def test_multiple_linked_projects(image_model: PrivateImage) -> None:
     """Verify `services` relationship works correctly.
 
-    Connect a multiple ComputeService to a Image.
+    Connect a multiple Project to a PrivateImage.
     """
     item = Project(**project_model_dict()).save()
-    private_image_model.projects.connect(item)
+    image_model.projects.connect(item)
     item = Project(**project_model_dict()).save()
-    private_image_model.projects.connect(item)
-    assert len(private_image_model.projects.all()) == 2
+    image_model.projects.connect(item)
+    assert len(image_model.projects.all()) == 2
--- tests/models/test_image_model_cases.py
+++ tests/models/test_image_model_cases.py
@@ -1,33 +1,95 @@
-from pytest_cases import case, parametrize
+from typing import Any
+from uuid import uuid4
+
+from pytest_cases import case
 
 from fed_reg.image.models import Image, PrivateImage, SharedImage
+from tests.utils import random_lower_string
 
 
-class CaseAttr:
-    @case(tags=("attr", "mandatory"))
-    @parametrize(value=("name", "uuid"))
-    def case_mandatory(self, value: str) -> str:
-        return value
+class CaseImageDict:
+    @case(tags=("dict", "mandatory", "valid"))
+    def case_mandatory(self) -> dict[str, Any]:
+        return {"name": random_lower_string(), "uuid": uuid4().hex}
 
-    @case(tags=("attr", "optional"))
-    @parametrize(
-        value=(
-            "description",
-            "os_type",
-            "os_distro",
-            "os_version",
-            "architecture",
-            "kernel_id",
-            "cuda_support",
-            "gpu_driver",
-            "tags",
-        )
-    )
-    def case_optional(self, value: str) -> str:
-        return value
+    @case(tags=("dict", "valid"))
+    def case_description(self) -> dict[str, Any]:
+        return {
+            "name": random_lower_string(),
+            "uuid": uuid4().hex,
+            "description": random_lower_string(),
+        }
+
+    @case(tags=("dict", "valid"))
+    def case_os_type(self) -> dict[str, Any]:
+        return {
+            "name": random_lower_string(),
+            "uuid": uuid4().hex,
+            "os_type": random_lower_string(),
+        }
+
+    @case(tags=("dict", "valid"))
+    def case_os_distro(self) -> dict[str, Any]:
+        return {
+            "name": random_lower_string(),
+            "uuid": uuid4().hex,
+            "os_distro": random_lower_string(),
+        }
+
+    @case(tags=("dict", "valid"))
+    def case_os_version(self) -> dict[str, Any]:
+        return {
+            "name": random_lower_string(),
+            "uuid": uuid4().hex,
+            "os_version": random_lower_string(),
+        }
+
+    @case(tags=("dict", "valid"))
+    def case_architecture(self) -> dict[str, Any]:
+        return {
+            "name": random_lower_string(),
+            "uuid": uuid4().hex,
+            "architecture": random_lower_string(),
+        }
+
+    @case(tags=("dict", "valid"))
+    def case_kernel_id(self) -> dict[str, Any]:
+        return {
+            "name": random_lower_string(),
+            "uuid": uuid4().hex,
+            "kernel_id": random_lower_string(),
+        }
+
+    @case(tags=("dict", "valid"))
+    def case_cuda_support(self) -> dict[str, Any]:
+        return {
+            "name": random_lower_string(),
+            "uuid": uuid4().hex,
+            "cuda_support": True,
+        }
+
+    @case(tags=("dict", "valid"))
+    def case_gpu_driver(self) -> dict[str, Any]:
+        return {"name": random_lower_string(), "uuid": uuid4().hex, "gpu_driver": True}
+
+    @case(tags=("dict", "valid"))
+    def case_tags(self) -> dict[str, Any]:
+        return {
+            "name": random_lower_string(),
+            "uuid": uuid4().hex,
+            "tags": [random_lower_string()],
+        }
+
+    @case(tags=("dict", "invalid"))
+    def case_missing_name(self) -> dict[str, Any]:
+        return {"uuid": uuid4().hex}
+
+    @case(tags=("dict", "invalid"))
+    def case_missing_uuid(self) -> dict[str, Any]:
+        return {"name": random_lower_string()}
 
 
-class CaseClass:
+class CaseImageModelClass:
     @case(tags="class")
     def case_image(self) -> type[Image]:
         return Image
@@ -41,17 +103,15 @@ class CaseClass:
         return SharedImage
 
 
-class CaseModel:
+class CaseImageModel:
     @case(tags="model")
-    def case_image_model(self, image_model: Image) -> Image:
-        return image_model
+    def case_image_model(self) -> Image:
+        return Image(name=random_lower_string(), uuid=uuid4().hex).save()
 
-    @case(tags="model")
-    def case_private_image_model(
-        self, private_image_model: PrivateImage
-    ) -> PrivateImage:
-        return private_image_model
+    @case(tags=("model", "private"))
+    def case_private_image_model(self) -> PrivateImage:
+        return PrivateImage(name=random_lower_string(), uuid=uuid4().hex).save()
 
-    @case(tags="model")
-    def case_shared_image_model(self, shared_image_model: SharedImage) -> SharedImage:
-        return shared_image_model
+    @case(tags=("model", "shared"))
+    def case_shared_image_model(self) -> SharedImage:
+        return SharedImage(name=random_lower_string(), uuid=uuid4().hex).save()
