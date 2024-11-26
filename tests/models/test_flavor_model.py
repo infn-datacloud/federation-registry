39e02cbe2b64245a2bbee211805106ba7b4e671f
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2024-11-26 11:22:36 +0000

    tests(flavor): update model tests

--- tests/models/test_flavor_model.py
+++ tests/models/test_flavor_model.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import pytest
 from neomodel import CardinalityViolation, RelationshipManager, RequiredProperty
 from pytest_cases import parametrize_with_cases
@@ -5,7 +7,7 @@ from pytest_cases import parametrize_with_cases
 from fed_reg.flavor.models import Flavor, PrivateFlavor, SharedFlavor
 from fed_reg.project.models import Project
 from fed_reg.service.models import ComputeService
-from tests.models.utils import flavor_model_dict, project_model_dict, service_model_dict
+from tests.models.utils import project_model_dict, service_model_dict
 
 
 @parametrize_with_cases("flavor_cls", has_tag=("class", "derived"))
@@ -17,31 +19,36 @@ def test_flavor_inheritance(
 
 
 @parametrize_with_cases("flavor_cls", has_tag="class")
-@parametrize_with_cases("attr", has_tag="attr")
-def test_flavor_attr(
-    flavor_cls: type[Flavor] | type[PrivateFlavor] | type[SharedFlavor], attr: str
+@parametrize_with_cases("data", has_tag=("dict", "valid"))
+def test_flavor_valid_attr(
+    flavor_cls: type[Flavor] | type[PrivateFlavor] | type[SharedFlavor],
+    data: dict[str, Any],
 ) -> None:
-    """Test attribute values (default and set).
+    """Test Flavor mandatory and optional attributes.
 
     Execute this test on Flavor, PrivateFlavor and SharedFlavor.
     """
-    d = flavor_model_dict(attr)
-    item = flavor_cls(**d)
+    item = flavor_cls(**data)
     assert isinstance(item, flavor_cls)
     assert item.uid is not None
-    assert item.description == d.get("description", "")
-    assert item.name == d.get("name")
-    assert item.uuid == d.get("uuid")
-    assert item.disk == d.get("disk", 0)
-    assert item.ram == d.get("ram", 0)
-    assert item.vcpus == d.get("vcpus", 0)
-    assert item.swap == d.get("swap", 0)
-    assert item.ephemeral == d.get("ephemeral", 0)
-    assert item.infiniband is d.get("infiniband", False)
-    assert item.gpus == d.get("gpus", 0)
-    assert item.gpu_model is d.get("gpu_model", None)
-    assert item.gpu_vendor is d.get("gpu_vendor", None)
-    assert item.local_storage is d.get("local_storage", None)
+    assert item.description == data.get("description", "")
+    assert item.name == data.get("name")
+    assert item.uuid == data.get("uuid")
+    assert item.disk == data.get("disk", 0)
+    assert item.ram == data.get("ram", 0)
+    assert item.vcpus == data.get("vcpus", 0)
+    assert item.swap == data.get("swap", 0)
+    assert item.ephemeral == data.get("ephemeral", 0)
+    assert item.infiniband is data.get("infiniband", False)
+    assert item.gpus == data.get("gpus", 0)
+    assert item.gpu_model is data.get("gpu_model", None)
+    assert item.gpu_vendor is data.get("gpu_vendor", None)
+    assert item.local_storage is data.get("local_storage", None)
+
+    if flavor_cls == SharedFlavor:
+        assert item.is_shared
+    if flavor_cls == PrivateFlavor:
+        assert not item.is_shared
 
     saved = item.save()
     assert saved.element_id_property
@@ -49,20 +56,17 @@ def test_flavor_attr(
 
 
 @parametrize_with_cases("flavor_cls", has_tag="class")
-@parametrize_with_cases("attr", has_tag=("attr", "mandatory"))
+@parametrize_with_cases("data", has_tag=("dict", "invalid"))
 def test_flavor_missing_mandatory_attr(
-    flavor_cls: type[Flavor] | type[PrivateFlavor] | type[SharedFlavor], attr: str
+    flavor_cls: type[Flavor] | type[PrivateFlavor] | type[SharedFlavor],
+    data: dict[str, Any],
 ) -> None:
-    """Test Flavor required attributes.
+    """Creating a model without required values raises a RequiredProperty error.
 
-    Creating a model without required values raises a RequiredProperty error.
     Execute this test on Flavor, PrivateFlavor and SharedFlavor.
     """
-    err_msg = f"property '{attr}' on objects of class {flavor_cls.__name__}"
-    d = flavor_model_dict()
-    d.pop(attr)
-    with pytest.raises(RequiredProperty, match=err_msg):
-        flavor_cls(**d).save()
+    with pytest.raises(RequiredProperty):
+        flavor_cls(**data).save()
 
 
 @parametrize_with_cases("flavor_model", has_tag="model")
@@ -79,6 +83,15 @@ def test_rel_def(flavor_model: Flavor | PrivateFlavor | SharedFlavor) -> None:
     assert flavor_model.services.definition
     assert flavor_model.services.definition["node_class"] == ComputeService
 
+    if isinstance(flavor_model, PrivateFlavor):
+        assert isinstance(flavor_model.projects, RelationshipManager)
+        assert flavor_model.projects.name
+        assert flavor_model.projects.source
+        assert isinstance(flavor_model.projects.source, PrivateFlavor)
+        assert flavor_model.projects.source.uid == flavor_model.uid
+        assert flavor_model.projects.definition
+        assert flavor_model.projects.definition["node_class"] == Project
+
 
 @parametrize_with_cases("flavor_model", has_tag="model")
 def test_required_rel(flavor_model: Flavor | PrivateFlavor | SharedFlavor) -> None:
@@ -92,6 +105,12 @@ def test_required_rel(flavor_model: Flavor | PrivateFlavor | SharedFlavor) -> No
         flavor_model.services.all()
     with pytest.raises(CardinalityViolation):
         flavor_model.services.single()
+
+    if isinstance(flavor_model, PrivateFlavor):
+        with pytest.raises(CardinalityViolation):
+            flavor_model.projects.all()
+        with pytest.raises(CardinalityViolation):
+            flavor_model.projects.single()
 
 
 @parametrize_with_cases("flavor_model", has_tag="model")
@@ -113,6 +132,23 @@ def test_single_linked_service(
     assert service.uid == compute_service_model.uid
 
 
+@parametrize_with_cases("flavor_model", has_tag=("model", "private"))
+def test_single_linked_project(
+    flavor_model: PrivateFlavor, project_model: Project
+) -> None:
+    """Verify `projects` relationship works correctly.
+
+    Connect a single Project to a PrivateFlavor.
+    """
+    r = flavor_model.projects.connect(project_model)
+    assert r is True
+
+    assert len(flavor_model.projects.all()) == 1
+    project = flavor_model.projects.single()
+    assert isinstance(project, Project)
+    assert project.uid == project_model.uid
+
+
 @parametrize_with_cases("flavor_model", has_tag="model")
 def test_multiple_linked_services(
     flavor_model: Flavor | PrivateFlavor | SharedFlavor,
@@ -129,66 +165,14 @@ def test_multiple_linked_services(
     assert len(flavor_model.services.all()) == 2
 
 
-def test_shared_flavor_default_attr() -> None:
-    """Test SharedFlavor specific attribute values."""
-    d = flavor_model_dict()
-    item = SharedFlavor(**d)
-    assert item.is_shared is True
-
-
-def test_private_flavor_default_attr() -> None:
-    """Test SharedFlavor specific attribute values and relationships definition."""
-    d = flavor_model_dict()
-    item = PrivateFlavor(**d)
-    assert item.is_shared is False
-
-
-def test_private_flavor_rel_def(private_flavor_model: PrivateFlavor) -> None:
-    """Test relationships definition."""
-    assert isinstance(private_flavor_model.projects, RelationshipManager)
-    assert private_flavor_model.projects.name
-    assert private_flavor_model.projects.source
-    assert isinstance(private_flavor_model.projects.source, PrivateFlavor)
-    assert private_flavor_model.projects.source.uid == private_flavor_model.uid
-    assert private_flavor_model.projects.definition
-    assert private_flavor_model.projects.definition["node_class"] == Project
-
-
-def test_private_flavor_required_rel(private_flavor_model: PrivateFlavor) -> None:
-    """Test Model required relationships.
-
-    A model without required relationships can exist but when querying those values, it
-    raises a CardinalityViolation error.
-    """
-    with pytest.raises(CardinalityViolation):
-        private_flavor_model.projects.all()
-    with pytest.raises(CardinalityViolation):
-        private_flavor_model.projects.single()
-
-
-def test_single_linked_project(
-    private_flavor_model: PrivateFlavor, project_model: Project
-) -> None:
+@parametrize_with_cases("flavor_model", has_tag=("model", "private"))
+def test_multiple_linked_projects(flavor_model: PrivateFlavor) -> None:
     """Verify `projects` relationship works correctly.
 
-    Connect a single Project to a PrivateFlavor.
-    """
-    r = private_flavor_model.projects.connect(project_model)
-    assert r is True
-
-    assert len(private_flavor_model.projects.all()) == 1
-    project = private_flavor_model.projects.single()
-    assert isinstance(project, Project)
-    assert project.uid == project_model.uid
-
-
-def test_multiple_linked_projects(private_flavor_model: PrivateFlavor) -> None:
-    """Verify `services` relationship works correctly.
-
-    Connect a multiple ComputeService to a Flavor.
+    Connect a multiple Project to a PrivateFlavor.
     """
     item = Project(**project_model_dict()).save()
-    private_flavor_model.projects.connect(item)
+    flavor_model.projects.connect(item)
     item = Project(**project_model_dict()).save()
-    private_flavor_model.projects.connect(item)
-    assert len(private_flavor_model.projects.all()) == 2
+    flavor_model.projects.connect(item)
+    assert len(flavor_model.projects.all()) == 2
--- tests/models/test_flavor_model_cases.py
+++ tests/models/test_flavor_model_cases.py
@@ -1,35 +1,109 @@
-from pytest_cases import case, parametrize
+from typing import Any
+from uuid import uuid4
+
+from pytest_cases import case
 
 from fed_reg.flavor.models import Flavor, PrivateFlavor, SharedFlavor
+from tests.utils import (
+    random_lower_string,
+    random_non_negative_int,
+    random_positive_int,
+)
 
 
-class CaseAttr:
-    @case(tags=("attr", "mandatory"))
-    @parametrize(value=("name", "uuid"))
-    def case_mandatory(self, value: str) -> str:
-        return value
+class CaseFlavorDict:
+    @case(tags=("dict", "mandatory", "valid"))
+    def case_mandatory(self) -> dict[str, Any]:
+        return {"name": random_lower_string(), "uuid": uuid4().hex}
 
-    @case(tags=("attr", "optional"))
-    @parametrize(
-        value=(
-            "description",
-            "disk",
-            "ram",
-            "vcpus",
-            "swap",
-            "ephemeral",
-            "infiniband",
-            "gpus",
-            "gpu_model",
-            "gpu_vendor",
-            "local_storage",
-        )
-    )
-    def case_optional(self, value: str) -> str:
-        return value
+    @case(tags=("dict", "valid"))
+    def case_description(self) -> dict[str, Any]:
+        return {
+            "name": random_lower_string(),
+            "uuid": uuid4().hex,
+            "description": random_lower_string(),
+        }
+
+    @case(tags=("dict", "valid"))
+    def case_disk(self) -> dict[str, Any]:
+        return {
+            "name": random_lower_string(),
+            "uuid": uuid4().hex,
+            "disk": random_positive_int(),
+        }
+
+    @case(tags=("dict", "valid"))
+    def case_ram(self) -> dict[str, Any]:
+        return {
+            "name": random_lower_string(),
+            "uuid": uuid4().hex,
+            "ram": random_positive_int(),
+        }
+
+    @case(tags=("dict", "valid"))
+    def case_vcpus(self) -> dict[str, Any]:
+        return {
+            "name": random_lower_string(),
+            "uuid": uuid4().hex,
+            "vcpus": random_positive_int(),
+        }
+
+    @case(tags=("dict", "valid"))
+    def case_swap(self) -> dict[str, Any]:
+        return {
+            "name": random_lower_string(),
+            "uuid": uuid4().hex,
+            "swap": random_positive_int(),
+        }
+
+    @case(tags=("dict", "valid"))
+    def case_ephemeral(self) -> dict[str, Any]:
+        return {
+            "name": random_lower_string(),
+            "uuid": uuid4().hex,
+            "ephemeral": random_positive_int(),
+        }
+
+    @case(tags=("dict", "valid"))
+    def case_gpus(self) -> dict[str, Any]:
+        return {
+            "name": random_lower_string(),
+            "uuid": uuid4().hex,
+            "gpus": random_positive_int(),
+        }
+
+    @case(tags=("dict", "valid"))
+    def case_gpu_model(self) -> dict[str, Any]:
+        return {
+            "name": random_lower_string(),
+            "uuid": uuid4().hex,
+            "gpus": random_non_negative_int(),
+            "gpu_model": random_lower_string(),
+        }
+
+    @case(tags=("dict", "valid"))
+    def case_gpu_vendor(self) -> dict[str, Any]:
+        return {
+            "name": random_lower_string(),
+            "uuid": uuid4().hex,
+            "gpus": random_non_negative_int(),
+            "gpu_vendor": random_lower_string(),
+        }
+
+    @case(tags=("dict", "valid"))
+    def case_infiniband(self) -> dict[str, Any]:
+        return {"name": random_lower_string(), "uuid": uuid4().hex, "infiniband": True}
+
+    @case(tags=("dict", "invalid"))
+    def case_missing_name(self) -> dict[str, Any]:
+        return {"uuid": uuid4().hex}
+
+    @case(tags=("dict", "invalid"))
+    def case_missing_uuid(self) -> dict[str, Any]:
+        return {"name": random_lower_string()}
 
 
-class CaseClass:
+class CaseFlavorModelClass:
     @case(tags="class")
     def case_flavor(self) -> type[Flavor]:
         return Flavor
@@ -43,19 +117,15 @@ class CaseClass:
         return SharedFlavor
 
 
-class CaseModel:
+class CaseFlavorModel:
     @case(tags="model")
-    def case_flavor_model(self, flavor_model: Flavor) -> Flavor:
-        return flavor_model
+    def case_flavor_model(self) -> Flavor:
+        return Flavor(name=random_lower_string(), uuid=uuid4().hex).save()
 
-    @case(tags="model")
-    def case_private_flavor_model(
-        self, private_flavor_model: PrivateFlavor
-    ) -> PrivateFlavor:
-        return private_flavor_model
+    @case(tags=("model", "private"))
+    def case_private_flavor_model(self) -> PrivateFlavor:
+        return PrivateFlavor(name=random_lower_string(), uuid=uuid4().hex).save()
 
-    @case(tags="model")
-    def case_shared_flavor_model(
-        self, shared_flavor_model: SharedFlavor
-    ) -> SharedFlavor:
-        return shared_flavor_model
+    @case(tags=("model", "shared"))
+    def case_shared_flavor_model(self) -> SharedFlavor:
+        return SharedFlavor(name=random_lower_string(), uuid=uuid4().hex).save()
