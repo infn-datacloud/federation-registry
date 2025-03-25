bc5e7c1fde936fd1a122c81e18b37ee2dd5fcfcc
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2025-03-25 09:38:21 +0000

    tests: Add user group CRUD tests

---
+++ tests/crud/test_user_group.py
@@ -0,0 +1,121 @@
+from uuid import uuid4
+
+import pytest
+from fedreg.identity_provider.models import IdentityProvider
+from fedreg.project.models import Project
+from fedreg.provider.models import Provider
+from fedreg.provider.schemas_extended import UserGroupCreateExtended
+from fedreg.user_group.models import UserGroup
+from pytest_cases import parametrize_with_cases
+
+from fed_reg.user_group.crud import user_group_mng
+from tests.utils import (
+    random_lower_string,
+    random_provider_type,
+    random_start_end_dates,
+    random_url,
+)
+
+
+@pytest.fixture
+def provider_model() -> Project:
+    """Provider model."""
+    return Provider(name=random_lower_string(), type=random_provider_type()).save()
+
+
+@pytest.fixture
+def identity_provider_model(provider_model: Provider) -> IdentityProvider:
+    """Identity provider model.
+
+    The parent identity provider is connected to the project's provider.
+    """
+    identity_provider = IdentityProvider(
+        endpoint=str(random_url()), group_claim=random_lower_string()
+    ).save()
+    provider_model.identity_providers.connect(
+        identity_provider,
+        {"protocol": random_lower_string(), "idp_name": random_lower_string()},
+    )
+    return identity_provider
+
+
+@pytest.fixture
+def user_group_model(identity_provider_model: IdentityProvider) -> UserGroup:
+    """User group model belonging to the same provider."""
+    user_group = UserGroup(name=random_lower_string()).save()
+    identity_provider_model.user_groups.connect(user_group)
+    return user_group
+
+
+class CaseUserGroup:
+    def case_user_group_create(self) -> UserGroupCreateExtended:
+        """This user group points to the identity_provider_model."""
+        start_date, end_date = random_start_end_dates()
+        doc_uuid = uuid4()
+        project = uuid4()
+        return UserGroupCreateExtended(
+            name=random_lower_string(),
+            # TODO: remove this attributes:
+            sla={
+                "start_date": start_date,
+                "end_date": end_date,
+                "doc_uuid": doc_uuid,
+                "project": project,
+            },
+        )
+
+
+@parametrize_with_cases("item", cases=CaseUserGroup)
+def test_create(
+    item: UserGroupCreateExtended, identity_provider_model: IdentityProvider
+) -> None:
+    """Create a new istance"""
+    db_obj = user_group_mng.create(
+        obj_in=item, identity_provider=identity_provider_model
+    )
+    assert db_obj is not None
+    assert isinstance(db_obj, UserGroup)
+    assert db_obj.identity_provider.is_connected(identity_provider_model)
+
+
+@parametrize_with_cases("item", cases=CaseUserGroup)
+def test_create_already_exists(
+    item: UserGroupCreateExtended, user_group_model: UserGroup
+) -> None:
+    """A user_group with the given uuid already exists"""
+    identity_provider = user_group_model.identity_provider.single()
+
+    item.name = user_group_model.name
+
+    msg = (
+        f"User group with name {item.name} already exists on identity provider "
+        f"{identity_provider.endpoint}."
+    )
+    with pytest.raises(AssertionError, match=msg):
+        user_group_mng.create(obj_in=item, identity_provider=identity_provider)
+
+
+@parametrize_with_cases("item", cases=CaseUserGroup)
+def test_update(item: UserGroupCreateExtended, user_group_model: UserGroup) -> None:
+    """Completely update the user group attributes. Also override not set ones."""
+    db_obj = user_group_mng.update(obj_in=item, db_obj=user_group_model)
+
+    assert db_obj is not None
+    assert isinstance(db_obj, UserGroup)
+    d = item.dict()
+    exclude_properties = ["uid", "element_id_property"]
+    for k in db_obj.__properties__.keys():
+        if k not in exclude_properties:
+            assert db_obj.__getattribute__(k) == d.get(k)
+
+
+@parametrize_with_cases("item", cases=CaseUserGroup)
+def test_update_no_changes(
+    item: UserGroupCreateExtended, user_group_model: UserGroup
+) -> None:
+    """The new item is equal to the existing one. No changes."""
+    item.name = user_group_model.name
+
+    db_obj = user_group_mng.update(obj_in=item, db_obj=user_group_model)
+
+    assert db_obj is None
